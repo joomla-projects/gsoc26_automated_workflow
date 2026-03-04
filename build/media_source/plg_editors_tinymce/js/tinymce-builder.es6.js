0ef92a88d43b5465339b5c5864928a45479dcbe8
Christian Heel <66922325+heelc29@users.noreply.github.com>
2026-03-03 20:55:17 -0600

    fix codestyle

--- build/media_source/plg_editors_tinymce/js/tinymce-builder.es6.js
+++ build/media_source/plg_editors_tinymce/js/tinymce-builder.es6.js
@@ -431,7 +431,8 @@ const TinyMCEBuilder = (container, options) => {
   });
 };
 
-const options = Joomla.getOptions ? Joomla.getOptions('plg_editors_tinymce_builder', {})
+const options = Joomla.getOptions
+  ? Joomla.getOptions('plg_editors_tinymce_builder', {})
   : (Joomla.optionsStorage.plg_editors_tinymce_builder || {});
 
 const builder = document.getElementById('joomla-tinymce-builder');
